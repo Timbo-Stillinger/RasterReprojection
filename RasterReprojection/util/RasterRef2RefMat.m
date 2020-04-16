70ae6c47f7402c6e983569a115c95ff1ad99a377
Jeff Dozier <dozier@ucsb.edu>
2020-04-15 21:17:08 -0700

    simplified via call to worldFileMatrix

--- RasterReprojection/util/RasterRef2RefMat.m
+++ RasterReprojection/util/RasterRef2RefMat.m
@@ -1,6 +1,6 @@
 function RefMat = RasterRef2RefMat(R)
 %R = input raster reference object
 %RefMat = output referencing matrix
-[x11,y11,dx,dy,~] = cornerCoords(R);
-RefMat = makerefmat(x11,y11,dx,dy);
+W = worldFileMatrix(R);
+RefMat = worldFileMatrixToRefmat(W);
 end
